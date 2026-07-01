62c59c7472c10e837a8f33be28075234fc957fa7
Алексей Боров <alsswsq1@gmail.com>
2026-07-01 16:32:39 +0400

    Реализован графический интерфейс

--- StartupAnalyzer/Form1.Designer.cs
+++ StartupAnalyzer/Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gridItems = new System.Windows.Forms.DataGridView();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridItems)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridItems
+            // 
+            this.gridItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridItems.Location = new System.Drawing.Point(92, 48);
+            this.gridItems.Name = "gridItems";
+            this.gridItems.Size = new System.Drawing.Size(1010, 286);
+            this.gridItems.TabIndex = 0;
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(92, 412);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(179, 58);
+            this.btnScan.TabIndex = 1;
+            this.btnScan.Text = "Сканировать\r\n";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(914, 412);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(188, 58);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Удалить выбранное\r\n";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1202, 516);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.gridItems);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridItems)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView gridItems;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Button btnDelete;
     }
 }
 
--- StartupAnalyzer/Form1.cs
+++ StartupAnalyzer/Form1.cs
@@ -1,20 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace StartupAnalyzer
 {
     public partial class Form1 : Form
     {
+        private SystemScanner scanner;
+        private StartupManager manager;
+        private SimpleLogger logger;
+        private List<StartupItem> currentItems;
+
         public Form1()
         {
             InitializeComponent();
+
+            scanner = new SystemScanner();
+            manager = new StartupManager();
+            logger = new SimpleLogger();
+
+            gridItems.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            gridItems.MultiSelect = false;
+            gridItems.ReadOnly = true;
+            gridItems.AllowUserToAddRows = false;
+
+            logger.LogAction("Запуск приложения", "Form1 загружена");
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            currentItems = scanner.GetStartupItems();
+
+            gridItems.DataSource = null;
+            gridItems.DataSource = currentItems;
+
+            gridItems.Columns["Name"].HeaderText = "Название программы";
+            gridItems.Columns["Path"].HeaderText = "Путь к файлу";
+            gridItems.Columns["Source"].HeaderText = "Где найдено";
+            gridItems.Columns["IsActive"].HeaderText = "Включена";
+
+            gridItems.Columns["Path"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+            logger.LogAction("Сканирование", $"Найдено записей: {currentItems.Count}");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (gridItems.SelectedRows.Count > 0)
+            {
+                var selectedItem = (StartupItem)gridItems.SelectedRows[0].DataBoundItem;
+
+                var confirmResult = MessageBox.Show(
+                    $"Вы уверены, что хотите удалить программу '{selectedItem.Name}' из автозагрузки?\n\nВнимание: Для некоторых записей могут потребоваться права администратора.",
+                    "Подтверждение удаления",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning);
+
+                if (confirmResult == DialogResult.Yes)
+                {
+                    bool success = manager.RemoveItem(selectedItem);
+                    if (success)
+                    {
+                        logger.LogAction("Удаление (Успех)", selectedItem.Name);
+                        MessageBox.Show("Программа успешно удалена из автозагрузки!", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        btnScan_Click(null, null);
+                    }
+                    else
+                    {
+                        logger.LogAction("Удаление (Ошибка)", selectedItem.Name);
+                        MessageBox.Show("Не удалось удалить запись. Возможно, не хватает прав администратора.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, сначала выберите программу в таблице.", "Внимание");
+            }
         }
     }
 }
